ff668edca5f5fbca3aa4ccfeb511d7854efef285
fernando <fernando_lopezc@live.com>
2019-02-06 10:04:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,14 +3,12 @@
 def countdown(number)
 while number>0 #While the seconds we passed as argument
   puts "#{number} SECOND(S)!"
-
+sleep number 
   number-=1
 end
 return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  sleep number
-end
+
 
   
